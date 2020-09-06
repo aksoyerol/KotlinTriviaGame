11cb904f026e4c5a65c69b2126eed8bddfab164f
pcparticle <erolaxoy98@gmail.com>
2020-09-06 16:33:22 +0300

    added-navigation-host

--- app/src/main/java/com/erolaksoy/kotlintriviagame/TitleFragment.kt
+++ app/src/main/java/com/erolaksoy/kotlintriviagame/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.erolaksoy.kotlintriviagame.databinding.FragmentTitleBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -29,6 +30,10 @@ class TitleFragment : Fragment() {
             container,
             false
         )
+
+        binding.button.setOnClickListener {view:View->
+                view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
 
         return binding.root
     }
